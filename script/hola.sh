444c709c32873d918fd91acfae3937576f275bb9
Ramryces2025 <dramirez@ceste.com>
2025-11-19 18:44:31 +0100

    Convert script to use Unix shell

--- script/hola.sh
+++ script/hola.sh
@@ -1,3 +1,2 @@
-@echo off
-echo Hola desde el repo en %DATE% %TIME%
-echo Resultado del build #%BUILD_NUMBER% > build-%BUILD_NUMBER%.txt
+echo "Hola desde el repo en $(date)"
+echo "Resultado del build #${BUILD_NUMBER}" > "build-${BUILD_NUMBER}.txt"
